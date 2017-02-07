ddd22ea39323c88e0c2ea1b5069d93512814de1d
Chance Nursey-Bush <cnurseyb@asu.edu>
2017-02-06 23:50:04 -0700

    Commit 2/6/17
    *launch wear app from mobile app done

--- mobile/src/main/java/com/unocode/androidweartest/MainActivity.java
+++ mobile/src/main/java/com/unocode/androidweartest/MainActivity.java
@@ -1,31 +1,26 @@
 package com.unocode.androidweartest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.wearable.DataApi;
-import com.google.android.gms.wearable.DataEvent;
-import com.google.android.gms.wearable.DataEventBuffer;
 import com.google.android.gms.wearable.MessageApi;
 import com.google.android.gms.wearable.Node;
 import com.google.android.gms.wearable.NodeApi;
 import com.google.android.gms.wearable.Wearable;
 
-public class MainActivity extends AppCompatActivity implements DataApi.DataListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
+import java.util.ArrayList;
+import android.support.annotation.NonNull;
 
-    Node mNode; // the connected device to send the message to
+public class MainActivity extends Activity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
     private GoogleApiClient mGoogleApiClient;
-    private boolean mResolvingError = false;
+
     public static final String START_ACTIVITY_PATH = "/start/MainActivity";
-    Button sendButton;
-    TextView tv1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,20 +32,10 @@ public class MainActivity extends AppCompatActivity implements DataApi.DataListe
                 .addOnConnectionFailedListener(this)
                 .build();
         mGoogleApiClient.connect();
-        sendButton = (Button) findViewById(R.id.btn_sendmessage);
-        sendButton.setOnClickListener( new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // TODO Auto-generated method stub
-                sendMessage();
-            }
-        });
-        tv1 = (TextView) findViewById(R.id.editText1);
     }
 
     @Override
     public void onConnected(Bundle bundle) {
-        resolveNode();
         Log.d("GoogleApi", "onConnected: " + bundle);
     }
 
@@ -60,65 +45,68 @@ public class MainActivity extends AppCompatActivity implements DataApi.DataListe
     }
 
     @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
         Log.d("GoogleApi", "onConnectionFailed: " + connectionResult);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        if (!mResolvingError) {
-            mGoogleApiClient.connect();
-        }
+
+        mGoogleApiClient.connect();
+    }
+
+    protected GoogleApiClient getGoogleApiClient() {
+        return mGoogleApiClient;
     }
 
     @Override
     protected void onStop() {
-        if (null != mGoogleApiClient && mGoogleApiClient.isConnected()) {
-            Wearable.DataApi.removeListener(mGoogleApiClient, this);
+        super.onStop();
+
+        if(mGoogleApiClient.isConnected()) {
             mGoogleApiClient.disconnect();
         }
-        super.onStop();
     }
 
-    @Override
-    public void onDataChanged(DataEventBuffer dataEvents) {
-        for (DataEvent event : dataEvents) {
-            if (event.getType() == DataEvent.TYPE_DELETED) {
-                Log.d("OnDataChanged", "DataItem deleted: " + event.getDataItem().getUri());
-            } else if (event.getType() == DataEvent.TYPE_CHANGED) {
-                Log.d("OnDataChanged", "DataItem changed: " + event.getDataItem().getUri());
-            }
-        }
-    }
-
-    private void sendMessage() {
-        if (mNode != null && mGoogleApiClient!=null && mGoogleApiClient.isConnected()) {
-            Wearable.MessageApi.sendMessage(
-                    mGoogleApiClient, mNode.getId(), START_ACTIVITY_PATH, null).setResultCallback(
-
-                    new ResultCallback<MessageApi.SendMessageResult>() {
-                        @Override
-                        public void onResult(MessageApi.SendMessageResult sendMessageResult) {
-
-                            if (!sendMessageResult.getStatus().isSuccess()) {
-                                Log.e("TAG", "Failed to send message with status code: "
-                                        + sendMessageResult.getStatus().getStatusCode());
-                            }
-                        }
-                    }
-            );
-        } else {
-            //Improve your code
-        }
-    }
-
-    private void resolveNode() {
-        Wearable.NodeApi.getConnectedNodes(mGoogleApiClient).setResultCallback(new ResultCallback<NodeApi.GetConnectedNodesResult>() {
+    public void startWear(View v){
+        getNodes(new OnGotNodesListener() {
             @Override
-            public void onResult(NodeApi.GetConnectedNodesResult getConnectedNodesResult) {
-                for (Node node : getConnectedNodesResult.getNodes()) {
-                    mNode = node;
+            public void onGotNodes(ArrayList<String> nodes) {
+                if(nodes.size() > 0) {
+                    sendMessage(nodes.get(0));
+                }
+            }
+        });
+    }
+
+    private void getNodes(final OnGotNodesListener cb) {
+        Wearable.NodeApi.getConnectedNodes(getGoogleApiClient()).setResultCallback(new ResultCallback<NodeApi.GetConnectedNodesResult>() {
+            @Override
+            public void onResult(@NonNull NodeApi.GetConnectedNodesResult nodes) {
+                ArrayList<String> results= new ArrayList<>();
+
+                for (Node node : nodes.getNodes()) {
+                    if(!results.contains(node.getId())) results.add(node.getId());
+                }
+
+                cb.onGotNodes(results);
+            }
+        });
+    }
+
+    public interface OnGotNodesListener {
+        void onGotNodes(ArrayList<String> nodes);
+    }
+
+
+    private void sendMessage(String node) {
+        Wearable.MessageApi.sendMessage(mGoogleApiClient , node , START_ACTIVITY_PATH , new byte[0]).setResultCallback(new ResultCallback<MessageApi.SendMessageResult>() {
+            @Override
+            public void onResult(@NonNull MessageApi.SendMessageResult sendMessageResult) {
+                if (!sendMessageResult.getStatus().isSuccess()) {
+                    Log.e("GoogleApi", "Failed to send message with status code: "
+                            + sendMessageResult.getStatus().getStatusCode());
                 }
             }
         });
